d42d214f588ca77d6344b416d7f70aa5c73b07d8
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-03-22 19:31:24 -0700

    add documentation

--- get_shards.py
+++ get_shards.py
@@ -1,13 +1,38 @@
-# adapted from:
-# https://tigress-web.princeton.edu/~pprice/ps1_pv3_3pi_20170110/README.txt
 
 from lsst.meas.algorithms.htmIndexer import HtmIndexer
-##from lsst.afw.coord import IcrsCoord
 from lsst.geom import degrees
 from lsst.geom import SpherePoint
 from astropy.table import Table
 
 def getShards(ra, dec, radius, depth=7):
+        '''
+        Get HTM trixel numbers within a given radius of a sky location.
+
+        Parameters
+        ----------
+            ra : float
+                Right Ascension in decimal degrees.
+            dec : float
+                Declination in decimal degrees.
+            radius : float
+                Radius within which to return nearby HTM trixel numbers.
+
+        Returns
+        -------
+            t : astropy.table.table.Table
+                Table with the list of HTM trixel numbers and corresponding
+                boolean flag indicating whether each trixel is on the boundary
+                of the search region.
+
+        Notes
+        -----
+            adapted from:
+            https://tigress-web.princeton.edu/~pprice/ps1_pv3_3pi_20170110/README.txt
+
+            takes ~0.001-0.002 seconds for a ~1 deg radius
+
+        '''
+
         htm = HtmIndexer(depth=depth)
 
         coord = SpherePoint(ra, dec, degrees)
