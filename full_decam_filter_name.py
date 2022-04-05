568706d7bb525a1753ff2e40ab77db6913561db7
Aaron Meisner <ameisner@fas.harvard.edu>
2022-04-04 23:11:48 -0700

    add a second utility to do conversion in the opposite direction

--- full_decam_filter_name.py
+++ full_decam_filter_name.py
@@ -24,3 +24,29 @@ def full_filter_name(filter):
                   'VR' : 'VR DECam c0007 6300.0 2600.0'}
 
     return full_names[filter]
+
+def abbrev_filter_name(filter):
+    '''
+    Retrieve abbreviated DECam filter name based on full DECam filter name.
+
+    Parameters
+    ----------
+        filter : str
+            Full DECam filter name.
+
+    Returns
+    -------
+        str
+            Abbreviated filter name.
+
+    '''
+
+    abbrev_names = {'u DECam c0006 3500.0 1000.0' : 'u',
+                    'g DECam SDSS c0001 4720.0 1520.0' : 'g',
+                    'r DECam SDSS c0002 6415.0 1480.0' : 'r',
+                    'i DECam SDSS c0003 7835.0 1470.0' : 'i',
+                    'z DECam SDSS c0004 9260.0 1520.0' : 'z',
+                    'Y DECam c0005 10095.0 1130.0' : 'Y',
+                    'VR DECam c0007 6300.0 2600.0' : 'VR'}
+
+    return abbrev_names[filter]
