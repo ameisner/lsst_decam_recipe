0c3da2bcda668a1ac6e4066dfcd1923f03a934c9
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-04-22 15:49:00 -0700

    example of using photoCalib object based on suggestion/example from Shenming

---
+++ photocalib.py
@@ -0,0 +1,17 @@
+import lsst.daf.persistence as dafPersist
+import numpy as np
+import astropy.io.fits as fits
+
+butler = dafPersist.Butler(inputs='DATA/rerun/processCcdOutputs')
+
+dataId = {'filter': 'g', 'visit': 769479, 'ccdnum': 10}
+calexp = butler.get('calexp', dataId)
+photoCalib = butler.get("calexp_photoCalib", **dataId)
+
+h = fits.getheader('DATA/2018-09-06/g/decam0769479.fits.fz')
+
+exptime = h['EXPTIME']
+
+MAGZERO = 2.5*np.log10(photoCalib.getInstFluxAtZeroMagnitude()/exptime)
+
+print(MAGZERO)
