27cb516df98bb9ccd5ed8a515d9618a5df4225a8
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-05-09 23:13:14 -0700

    initial; need to add write-out

---
+++ dummy_mastercal_hdu.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import astropy.io.fits as fits
+import sys
+import os
+
+#fname_in = '/data0/ameisner/maxref_failures/flats_biases/c4d_180905_193713_zci_v1.fits.fz'
+
+fname_in = sys.argv[1]
+
+assert(os.path.exists(fname_in))
+
+hdul = fits.open(fname_in)
+
+_hdul = hdul[0:61]
+
+_hdul.append(_hdul[60])
+_hdul.append(hdul[61])
+
+for i, hdu in enumerate(_hdul):
+    if 'CCDNUM' in hdu.header:
+        print(i, hdu.header['CCDNUM'], i == hdu.header['CCDNUM'])
+
