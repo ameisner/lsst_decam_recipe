f65a0c037c370c4d7923dc00a448664cb3cfc542
Aaron Meisner <aaron.meisner@noirlab.edu>
2022-06-20 21:06:39 -0700

    convert to stacked bar chart

--- count_calexp.py
+++ count_calexp.py
@@ -36,8 +36,47 @@ def hist_by_ccdnum(basedir):
         tokens = os.path.basename(f).split('_')
         ccdnums[i] = int(tokens[1][0:2])
 
-    bins = np.arange(62)
-    plt.hist(ccdnums, bins)
+    bins = np.arange(63) + 0.5
+    counts_good, bins, _ = plt.hist(ccdnums, bins)
+    print(counts_good)
+    print(bins)
+    assert(np.sum(counts_good) == len(flist_calexp))
+
+    counts_bad = n_exp - counts_good
+
+    plt.cla()
+
+    # stacked bar chart
+
+    # https://matplotlib.org/stable/gallery/lines_bars_and_markers/bar_stacked.html#sphx-glr-gallery-lines-bars-and-markers-bar-stacked-py
+
+    fig, ax = plt.subplots(figsize=(12, 4))
+
+    labels = np.arange(1, 63)
+    width = 0.8
+    ax.bar(labels, counts_good, width, label='success')
+    ax.bar(labels, counts_bad, width, bottom=counts_good,
+           label='failure')
+
+    #ax.set_ylabel('Scores')
+    #ax.set_title('Scores by group and gender')
+    ax.legend(loc='upper right')
+
+    plt.ylim((0, n_exp*1.25))
+
+    xlim = (-2, 65)
+
+    plt.plot(xlim, [n_exp]*2, c='gray', linestyle=':')
+    plt.xlim(xlim)
+
+    plt.xticks(np.arange(1, 63), rotation=90)
+
+    title = basedir + ' ; ' + str(n_exp) + ' exposures'
+    plt.title(title)
+
+    plt.xlabel('CCDNUM')
+    plt.ylabel('# of CCDs')
+
     plt.show()
 
 def _test():
